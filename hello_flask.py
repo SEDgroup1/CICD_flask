0054d47bae1b2e61dafdf5d98fdfa8499d673adf
pcchencode <zxp791228@gmail.com>
2020-12-09 17:40:12 +0800

    add docker file

--- hello_flask.py
+++ hello_flask.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return 'hello man'
 
-
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8888)
+    # docker run -p 80:8888 flask_im

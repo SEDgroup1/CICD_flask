b7f6f30b026dfdc7eb55ad7df5aac9165c656a7f
pcchencode <zxp791228@gmail.com>
2020-12-13 21:37:51 +0800

    update

--- hello_flask.py
+++ hello_flask.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     app.debug = True
     # app.run(debug=True, host='0.0.0.0', port=8888)
     app.run(debug=True, host='0.0.0.0', port=5000)
-    # docker run -p 80:8888 flask_im, -p 將主機 8888 port 與 container的 80 port 綁定
+    # docker run -p 80:8888 flask_img, -p 將主機 8888 port 與 container的 80 port 綁定
     # http://0.0.0.0:80/
 
-    # docker run - p 80:5000
-    # http://0.0.0.0:80/
+    # docker run  f-p 80:5000 flask_img
+    # kubectl port-forward kubernetes-demo-pod 2000:5000
+    # http://0.0.0.0:2000/

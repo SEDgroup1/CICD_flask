10e47bbecedac1756ed660e32bb03bee031c6306
pcchencode <zxp791228@gmail.com>
2020-12-13 22:52:18 +0800

    updat3

--- hello_flask.py
+++ hello_flask.py
@@ -9,7 +9,7 @@ def index():
 if __name__ == '__main__':
     app.debug = True
     # app.run(debug=True, host='0.0.0.0', port=8888)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000) # 指定本地端用 port=5000 連接
     # docker run -p 80:8888 flask_img, -p 將主機 8888 port 與 container的 80 port 綁定
     # http://0.0.0.0:80/
 

7417b58e8b235836f2ab2de3f3590ba410dc15bd
pcchencode <zxp791228@gmail.com>
2020-12-16 17:05:08 +0800

    update

--- hello_flask.py
+++ hello_flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello hello hellp123123 CICD'
+    return 'hello hello hellp123123 CICD, JERRY'
 
 if __name__ == '__main__':
     app.debug = True
